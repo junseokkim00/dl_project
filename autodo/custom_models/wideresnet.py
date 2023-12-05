59c896cfed71f7cc835ff9f044fc1904b1a0aa38
root <lalswo010@postech.ac.kr>
2023-12-05 18:58:44 +0000

    3:53

--- autodo/custom_models/wideresnet.py
+++ autodo/custom_models/wideresnet.py
@@ -93,6 +93,109 @@ model_dict = {
 }
 
 
+class ProjectionHead(nn.Module):
+    """Base class for all projection and prediction heads.
+
+    Args:
+        blocks:
+            List of tuples, each denoting one block of the projection head MLP.
+            Each tuple reads (in_features, out_features, batch_norm_layer,
+            non_linearity_layer).
+
+    Examples:
+        >>> # the following projection head has two blocks
+        >>> # the first block uses batch norm an a ReLU non-linearity
+        >>> # the second block is a simple linear layer
+        >>> projection_head = ProjectionHead([
+        >>>     (256, 256, nn.BatchNorm1d(256), nn.ReLU()),
+        >>>     (256, 128, None, None)
+        >>> ])
+
+    """
+
+    def __init__(
+        self, blocks: List[Tuple[int, int, Optional[nn.Module], Optional[nn.Module]]]
+    ) -> None:
+        super(ProjectionHead, self).__init__()
+
+        layers: List[nn.Module] = []
+        for input_dim, output_dim, batch_norm, non_linearity in blocks:
+            use_bias = not bool(batch_norm)
+            layers.append(nn.Linear(input_dim, output_dim, bias=use_bias))
+            if batch_norm:
+                layers.append(batch_norm)
+            if non_linearity:
+                layers.append(non_linearity)
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Computes one forward pass through the projection head.
+
+        Args:
+            x:
+                Input of shape bsz x num_ftrs.
+
+        """
+        projection: Tensor = self.layers(x)
+        return projection
+
+class SimCLRProjectionHead(ProjectionHead):
+    """Projection head used for SimCLR.
+
+    "We use a MLP with one hidden layer to obtain zi = g(h) = W_2 * σ(W_1 * h)
+    where σ is a ReLU non-linearity." [0]
+
+    "We use a 3-layer MLP projection head on top of a ResNet encoder." [1]
+
+    - [0] SimCLR v1, 2020, https://arxiv.org/abs/2002.05709
+    - [1] SimCLR v2, 2020, https://arxiv.org/abs/2006.10029
+    """
+
+    def __init__(
+        self,
+        input_dim: int = 2048,
+        hidden_dim: int = 2048,
+        output_dim: int = 128,
+        num_layers: int = 2,
+        batch_norm: bool = True,
+    ):
+        """Initialize a new SimCLRProjectionHead instance.
+
+        Args:
+            input_dim: Number of input dimensions.
+            hidden_dim: Number of hidden dimensions.
+            output_dim: Number of output dimensions.
+            num_layers: Number of hidden layers (2 for v1, 3+ for v2).
+            batch_norm: Whether or not to use batch norms.
+        """
+        layers: List[Tuple[int, int, Optional[nn.Module], Optional[nn.Module]]] = []
+        layers.append(
+            (
+                input_dim,
+                hidden_dim,
+                nn.BatchNorm1d(hidden_dim) if batch_norm else None,
+                nn.ReLU(),
+            )
+        )
+        for _ in range(2, num_layers):
+            layers.append(
+                (
+                    hidden_dim,
+                    hidden_dim,
+                    nn.BatchNorm1d(hidden_dim) if batch_norm else None,
+                    nn.ReLU(),
+                )
+            )
+        layers.append(
+            (
+                hidden_dim,
+                output_dim,
+                nn.BatchNorm1d(output_dim) if batch_norm else None,
+                None,
+            )
+        )
+        super().__init__(layers)
+
 class SupConWideResNet(nn.Module):
     """projection head"""
     def __init__(self, name='wresnet28_10', num_classes=10, head='mlp', feat_dim=128):
@@ -120,9 +223,10 @@ class SupCeWideResNet(nn.Module):
     def __init__(self, name='wresnet28_10', num_classes=10):
         super(SupCeWideResNet, self).__init__()
         _, dim_in = model_dict[name]
-        self.cls = nn.Linear(dim_in, num_classes)
+        # Need to be modified depend on input dim_size 
+        self.projection_head = SimCLRProjectionHead(dim_in, 512, 128)
 
     def forward(self, x):
-        y = self.cls(x)
-        return [y]
+        z = self.projection_head(x)
+        return z
 
